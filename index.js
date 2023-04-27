c70cc387080507f4bf6724a4f2a62f0bcff70b08
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-04-27 19:03:28 +0530

    express app with crud operation

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const app = express();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+
+app.use(express.json());
+
+
+// sending all products to the user using get method
+app.get('/products', async (req, res) => {
+
+    const products = JSON.parse(await fsPromises.readFile(path.join(__dirname, "Data", 'data.json'), 'utf8'));
+    res.status(200).json(products);
+})
+
+
+// sending specified product to the user using get method 
+app.get('/products/:id', async (req, res) => {
+
+    const products = JSON.parse(await fsPromises.readFile(path.join(__dirname, "Data", 'data.json'), 'utf8'));
+    const found = products.find((product) => product.id === +req.params.id);
+    if (!found) {
+        res.status(404).json({ "message": `Not found product with id ${req.params.id}` });
+    }
+    res.status(200).json(found);
+})
+
+
+// adding new product to the db using post method
+app.post('/products', async (req, res) => {
+
+    let products = JSON.parse(await fsPromises.readFile(path.join(__dirname, "Data", 'data.json'), 'utf8'));
+    products = [...products, { "id": products.length + 1, ...req.body }]
+    console.log(req.body)
+    await fsPromises.writeFile(path.join(__dirname, 'data', 'data.json'), JSON.stringify(products));
+    res.status(201).json({ "message": "successfull added" })
+})
+
+
+// updating product by replacing the entire product details with users data using put method
+app.put('/products/:id', async (req, res) => {
+
+    const id = +req.params.id;
+    let products = JSON.parse(await fsPromises.readFile(path.join(__dirname, "Data", 'data.json'), 'utf8'));
+
+    const foundIndex = products.findIndex((product) => product.id===id);
+    if(foundIndex===-1) {
+       return res.status(404).json({"message":"Not found data"});
+    }
+
+    products.splice(foundIndex,1,{"id":id,...req.body})
+    await fsPromises.writeFile(path.join(__dirname,"Data","data.json"),JSON.stringify(products));
+    res.status(200).json({"message":"succussfully Updated Data","data":{...req.body}})
+
+})
+
+
+// updating only some property of given product id using patch method 
+app.patch('/products/:id', async (req,res) => {
+
+    const id = +req.params.id;
+    let products = JSON.parse(await fsPromises.readFile(path.join(__dirname, "Data", 'data.json'), 'utf8'));
+
+    const foundIndex = products.findIndex((product) => product.id === id);
+    if (foundIndex === -1) {
+        return res.status(404).json({ "message": "Not found data" });
+    }
+    const data = products[foundIndex];
+
+    products.splice(foundIndex, 1, { ...data, ...req.body })
+    await fsPromises.writeFile(path.join(__dirname, "Data", "data.json"), JSON.stringify(products));
+    res.status(200).json({ "message": "succussfully Updated Data", "data": { ...req.body } })
+})
+
+
+// deleting product by given product id using delete method
+app.delete('/products/:id', async (req,res) => {
+    
+    const id = +req.params.id;
+    const products = JSON.parse(await fsPromises.readFile(path.join(__dirname,"Data","data.json"),'utf8'));
+
+    const found = products.find((product) => product.id === id);
+    if(!found) {
+        return res.status(404).json({"message":`Not found any product with given id ${id}`})
+    }
+
+    const afterDelete = products.filter((product) => product.id!==id);
+    await fsPromises.writeFile(path.join(__dirname,"Data","data.json"),JSON.stringify(afterDelete));
+    res.status(200).json({"message":`Successfully deleted product with given id ${id}`,"product":{...found}})
+})
+
+
+
+
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
+})
